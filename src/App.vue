cbf8542d6a7822f088290347f701f3239dfa57da
Benjamin Kooma <benjaminkooma@Benjamin-Kooma.local>
2022-02-08 12:34:01 +0300

    finish sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script>
 import {
   BadgeCheckIcon,
-  Circle
+  PlusIcon,
 } from '@heroicons/vue/outline';
 export default {
   components: {
-    BadgeCheckIcon
+    BadgeCheckIcon,
+    PlusIcon,
   },
 };
 </script>
 
 <template>
 <div class="flex w-screen h-screen">
-  <div class="bg-gray-100 h-full w-1/4">
+  <div class="bg-gray-100 h-full w-1/4 relative overflow-auto no-scroll-bar p-0">
     <!-- name bar -->
     <div class="flex p-5">
       <div class="h-16 w-16 bg-red-500 rounded-full shadow-lg shadow-red-500/50"></div>
@@ -56,12 +57,48 @@ export default {
         <span class="h-12 w-2 rounded-tr-full rounded-br-full bg-red-600 invisible"></span>
         <div class="flex w-full items-center">
           <!-- icon -->
-          <div class="mx-3 h-8 w-8 font-normal borfer-2"></div>
+          <div class="mx-3 h-6 w-6 font-normal border-2 border-yellow-400 rounded-full"></div>
           <!-- name -->
           <div class="text-gray-400">Rescehedule Design</div>
         </div>
       </div>
+      <div class="w-full h-16 p-0 flex items-center">
+        <span class="h-12 w-2 rounded-tr-full rounded-br-full bg-red-600 invisible"></span>
+        <div class="flex w-full items-center">
+          <!-- icon -->
+          <div class="mx-3 h-6 w-6 font-normal border-2 border-cyan-400 rounded-full"></div>
+          <!-- name -->
+          <div class="text-gray-400">Layout Scaffolding</div>
+        </div>
+      </div>
+      <div class="w-full h-16 p-0 flex items-center">
+        <span class="h-12 w-2 rounded-tr-full rounded-br-full bg-red-600 invisible"></span>
+        <div class="flex w-full items-center">
+          <!-- icon -->
+          <div class="mx-3 h-6 w-6 font-normal border-2 border-green-400 rounded-full"></div>
+          <!-- name -->
+          <div class="text-gray-400">UX Testing</div>
+        </div>
+      </div>
+      <div class="w-full h-16 p-0 flex items-center">
+        <span class="h-12 w-2 rounded-tr-full rounded-br-full bg-red-600 invisible"></span>
+        <div class="flex w-full items-center">
+          <!-- icon -->
+          <div class="mx-3 h-6 w-6 font-normal border-2 border-purple-400 rounded-full"></div>
+          <!-- name -->
+          <div class="text-gray-400">Scaling</div>
+        </div>
+      </div>
     </div>
+
+    <!-- footer -->
+    <div class="p-5 flex flex-col" v-for="item in [1,2,3,3,3,2]" :key="item">
+      <div class="h-16 w-16 bg-gradient-to-r from-red-500 via-red-600 to-red-600 rounded-full flex items-center justify-center">
+        <PlusIcon class="text-white h-8 w-8" />
+      </div>
+    </div>
+
+    <div class="text-gray-400 mt-5 font-bold text-sm fixed bottom-0 right-0">2022 UI/UX Designs ui.com</div>
 
   </div>
   <div class="bg-white border-r border-l w-1/2"></div>
